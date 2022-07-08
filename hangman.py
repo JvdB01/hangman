bcc35eb7ca7a95e27189b6a4388b416cc0cf746e
JvdB01 <julien.vandenberk192@gmail.com>
2022-07-08 13:41:19 +0200

    made the game functional and now has custom words

--- hangman.py
+++ hangman.py
@@ -1,3 +1,17 @@
+import random
+import subprocess
+import gallow
+
+def draw_screen(known, mistakes):
+    subprocess.call("clear")
+    gallow.draw(mistakes)
+    buff = ""
+    for n in known:
+        buff += n
+        buff += " "
+    print(buff)
+
+subprocess.call("clear")
 print("Choose your difficulty:")
 print("(1) Low (Cowardly, but a safe bet.)")
 print("(2) Medium (Living on the edge?)")
@@ -17,4 +31,36 @@ if difficulty == 3:
 if difficulty == 4:
     f =  open('custom', 'r')
     wordlist =f.read().split('\n')
-print(wordlist)
+
+mistakes = 0
+word = random.choice(wordlist)
+length = len(word)
+known = ""
+while length > 0:
+    known += "_"
+    length -= 1
+
+while mistakes < 9:
+    draw_screen(known, mistakes)
+    if known == word:
+        print("You win!")
+        break
+    guess = input("Guess a letter/word: ")
+    if len(guess) == 1:
+        if guess in word:
+            matches = []
+            for pos, char in enumerate(word):
+                if char == guess:
+                    matches.append(pos)
+            for match in matches:
+                known = known[0:match]+guess+known[match+1: ]
+        else:
+            mistakes += 1
+    else:
+        if guess == word:
+            known = word
+        else:
+            mistakes += 1
+if known != word:
+    draw_screen(known, mistakes)
+    print("You lose!")
